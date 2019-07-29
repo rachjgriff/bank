7dfed94c7089fa744742f840ed5d486bbb952ad9
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-07-29 22:59:42 +0100

    REFACTOR: Removed before(each:), replaced with subject(:bank_account) REFACTOR: Removed @transaction from initialization USER STORY: Transaction History

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -4,23 +4,30 @@ class BankAccount
 
   def initialize
     @balance = 0
-    @transaction = { date: "", credit: 0, debit: 0, balance: 0 }
     @transaction_history = []
   end
 
   def deposit(date:, credit:)
     @balance += credit
 
+    @transaction = {}
     @transaction[:date] = date
     @transaction[:credit] = credit
+    @transaction[:debit] = 0
     @transaction[:balance] = @balance
   end
 
   def withdrawal(date:, debit:)
     @balance -= debit
 
+    @transaction = {}
     @transaction[:date] = date
+    @transaction[:credit] = 0
     @transaction[:debit] = debit
     @transaction[:balance] = @balance
+  end
+
+  def record_transaction
+    @transaction_history << @transaction
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -2,49 +2,65 @@ require 'bank_account'
 
 describe BankAccount do
 
-  before(:each) do
-    @bank_account = BankAccount.new
-  end
+  subject(:bank_account) { described_class.new }
 
   context 'When a bank account is setup' do
     it 'Balance is set to 0' do
-      expect(@bank_account.balance).to eq 0
-    end
-
-    it 'There is no existing transaction' do
-      expect(@bank_account.transaction[:date]).to eq ""
-      expect(@bank_account.transaction[:credit]).to eq 0
-      expect(@bank_account.transaction[:debit]).to eq 0
-      expect(@bank_account.transaction[:balance]).to eq 0
+      expect(bank_account.balance).to eq 0
     end
 
     it 'There is no transaction history' do
-      expect(@bank_account.transaction_history).to eq []
+      expect(bank_account.transaction_history).to eq []
     end
   end
 
   context 'As a bank account holder, I can:' do
     it 'Deposit money into a bank account' do
-      @bank_account.deposit(date: "10-01-2012", credit: 1000)
-      expect(@bank_account.balance).to eq 1000
+      bank_account.deposit(date: "10-01-2012", credit: 1000)
+
+      expect(bank_account.balance).to eq 1000
     end
 
     it 'Withdraw money from a bank account' do
-      @bank_account.deposit(date: "10-01-2012", credit: 1000)
-      @bank_account.withdrawal(date: "10-01-2012", debit: 500)
-      expect(@bank_account.balance).to eq 500
+      bank_account.deposit(date: "10-01-2012", credit: 1000)
+      bank_account.withdrawal(date: "14-01-2012", debit: 500)
+
+      expect(bank_account.balance).to eq 500
+    end
+  end
+
+  context 'Account holder transactions are stored' do
+    it 'Current transaction' do
+      bank_account.deposit(date: "10-01-2012", credit: 1000)
+
+      expect(bank_account.transaction[:date]).to eq "10-01-2012"
+      expect(bank_account.transaction[:credit]).to eq 1000
+      expect(bank_account.transaction[:debit]).to eq 0
+      expect(bank_account.transaction[:balance]).to eq 1000
     end
 
-    it 'Track my current transaction' do
-      @bank_account.deposit(date: "10-01-2012", credit: 1000)
-      expect(@bank_account.transaction[:date]).to eq "10-01-2012"
-      expect(@bank_account.transaction[:credit]).to eq 1000
-      expect(@bank_account.transaction[:debit]).to eq 0
-      expect(@bank_account.transaction[:balance]).to eq 1000
+    it 'Current transaction recorded in transaction history' do
+      bank_account.deposit(date: "10-01-2012", credit: 1000)
+      bank_account.record_transaction
+
+      expect(bank_account.transaction_history[0][:date]).to eq "10-01-2012"
+      expect(bank_account.transaction_history[0][:credit]).to eq 1000
+      expect(bank_account.transaction_history[0][:debit]).to eq 0
+      expect(bank_account.transaction_history[0][:balance]).to eq 1000
     end
 
-    # it 'See my transaction history' do
-    #
-    # end
+    it 'Multiple transactions are recorded in transaction history' do
+      bank_account.deposit(date: "10-01-2012", credit: 1000)
+      bank_account.record_transaction
+      bank_account.deposit(date: "13-01-2012", credit: 2000)
+      bank_account.record_transaction
+      bank_account.withdrawal(date: "14-01-2012", debit: 500)
+      bank_account.record_transaction
+
+      expect(bank_account.transaction_history[1][:date]).to eq "13-01-2012"
+      expect(bank_account.transaction_history[2][:credit]).to eq 0
+      expect(bank_account.transaction_history[0][:debit]).to eq 0
+      expect(bank_account.transaction_history[2][:balance]).to eq 2500
+    end
   end
 end
