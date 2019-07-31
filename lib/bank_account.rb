5d1b86d7a807a41a28e114ff94f4615a4ea6ff0a
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-07-31 08:44:52 +0100

    REFACTOR: Created private methods for transactions to adhere to SRP in public methods

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -12,9 +12,25 @@ class BankAccount
 
   def deposit(date:, credit:)
     @balance += credit
+    deposit_transaction(date, credit)
+  end
 
-    # Move to private as deposit_transaction
-    # Test what deposit_transaction returns
+  def withdrawal(date:, debit:)
+    @balance -= debit
+    withdrawal_transaction(date, debit)
+  end
+
+  def record_transaction
+    @transaction_history << @transaction
+  end
+
+  def print_bank_statement
+    @bank_statement.create_bank_statement(@transaction_history)
+  end
+
+  private
+
+  def deposit_transaction(date, credit)
     @transaction = {}
     @transaction[:date] = date
     @transaction[:credit] = '%.2f' % credit
@@ -22,24 +38,11 @@ class BankAccount
     @transaction[:balance] = '%.2f' % @balance
   end
 
-  def withdrawal(date:, debit:)
-    @balance -= debit
-
-    # Move to private as withdrawal_transaction
-    # Test what deposit_transaction returns
+  def withdrawal_transaction(date, debit)
     @transaction = {}
     @transaction[:date] = date
     @transaction[:credit] = ""
     @transaction[:debit] = '%.2f' % debit
     @transaction[:balance] = '%.2f' % @balance
-  end
-
-  def record_transaction
-    @transaction_history << @transaction
-  end
-
-  # ADD METHOD TO PRINT STATEMENT - @bank_statement.print_bank_statement
-  def print_bank_statement
-    @bank_statement.create_bank_statement(@transaction_history)
   end
 end
